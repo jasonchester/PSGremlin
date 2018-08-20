4fb8e5f708e045f8a82e79ac260bdf8692c9a481
Jason Chester <jason.chester@outlook.com>
2018-08-20 12:19:24 -0400

    cleanup test creds

--- src/PSGremlin.psm1
+++ src/PSGremlin.psm1
@@ -1,1 +1,22 @@
 # For future use
+# Load your target version of the assembly
+$dep = [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\System.Reflection.TypeExtensions.dll")
+$onAssemblyResolveEventHandler = [System.ResolveEventHandler] {
+  param($sender, $e)
+  # You can make this condition more or less version specific as suits your requirements
+  if ($e.Name.StartsWith("System.Reflection.TypeExtensions")) {
+    return $dep
+  }
+  foreach($assembly in [System.AppDomain]::CurrentDomain.GetAssemblies()) {
+    if ($assembly.FullName -eq $e.Name) {
+      return $assembly
+    }
+  }
+  return $null
+}
+[System.AppDomain]::CurrentDomain.add_AssemblyResolve($onAssemblyResolveEventHandler)
+
+# Rest of your script....
+
+# # Detach the event handler (not detaching can lead to stack overflow issues when closing PS)
+# [System.AppDomain]::CurrentDomain.remove_AssemblyResolve($onAssemblyResolveEventHandler)
