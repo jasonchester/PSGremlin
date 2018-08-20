579ca39ac69c75fd2afb107de3605a099717e6ea
Jason chester <jason.chester@brandmuscle.com>
2018-08-20 13:22:23 -0400

    add test for cosmos

---
+++ test/Test-Cosmos.ps1
@@ -0,0 +1,15 @@
+
+[CmdletBinding()]
+param (
+    [PSCredential] $creds = (Get-Credential "/dbs/dev01/colls/ldm")
+)
+
+
+Import-Module $(Join-Path $PSScriptRoot "../Publish/PSGremlin.dll")
+Get-Command -Module PSGremlin
+
+$queries = @(
+    "g.V().has('tenantId','ditchwitch')", "g.E().has('tenantId','ditchwitch')"
+)
+
+$queries | Invoke-Gremlin -Hostname "dev01-graph01.gremlin.cosmosdb.azure.com" -Credential $creds -EnableSsl $true -Cosmos | ConvertTo-Json -Depth 5
