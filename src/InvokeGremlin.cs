9881a2e6a57e4667eeccae70aa87934df3e65d6b
Jason chester <jason.chester@brandmuscle.com>
2018-10-23 15:15:03 -0400

    Nuget Update, Working on PS Desktop now I think

--- src/InvokeGremlin.cs
+++ src/InvokeGremlin.cs
@@ -1,29 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Management.Automation;
 using System.Threading.Tasks;
 using Gremlin.Net.Driver;
 using Gremlin.Net.Structure.IO.GraphSON;
+using Newtonsoft.Json.Linq;
 
 namespace PSGremlin
 {
     [Cmdlet(VerbsLifecycle.Invoke, "Gremlin")]
     public class InvokeGremlin : PSCmdlet
     {
-        [Parameter(Mandatory=true)]
+        [Parameter(Mandatory = true)]
         public string Hostname { get; set; }
 
         [Parameter]
         public int Port { get; set; } = 443;
 
         [Parameter]
-        public bool EnableSsl { get; set; } = true;
+        public SwitchParameter EnableSsl { get; set; } = true;
 
-        [Parameter(Mandatory=true)]        
+        [Parameter(Mandatory = true)]
         public PSCredential Credential { get; set; }
         // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
 
-        [Parameter(Mandatory=true, ValueFromPipeline=true)]
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
         public string Query;
 
         private GremlinClient Client;
@@ -42,15 +44,49 @@ namespace PSGremlin
             var sw = new Stopwatch();
             WriteVerbose($"Executing GremlinQuery {Query}");
             sw.Start();
-            var output = ExecuteQuery<dynamic>(Query).Result;
+            var results = ExecuteQuery<dynamic>(Query).Result;
             sw.Stop();
             WriteVerbose($"Query Complete. {sw.Elapsed}");
-            WriteObject(output, true);
+
+            foreach (var result in results)
+            {
+                PSObject output;
+
+                // for my own reference
+                // https://docs.microsoft.com/en-us/dotnet/csharp/pattern-matching#using-pattern-matching-switch-statements
+                switch (result)
+                {
+                    // case Dictionary<string, object> resultDict:
+                    //     WriteVerbose($"Processing type case 1: {result.GetType()}");
+
+                    //     output = new PSObject();
+
+                    //     output.Properties.Add(new PSNoteProperty("id", resultDict["id"]));
+                    //     output.Properties.Add(new PSNoteProperty("type", resultDict["type"]));
+                    //     output.Properties.Add(new PSNoteProperty("label", resultDict["label"]));
+
+                    //     var properties = resultDict["properties"] as Dictionary<string,object>;
+                    //     if(null != properties)
+                    //     foreach(var name in properties.Keys)
+                    //     {
+                    //         output.Properties.Add(new PSNoteProperty(name, properties[name]));
+                    //     }
+                    //     break;
+
+                    default:
+                        WriteVerbose($"Processing default case: {result.GetType()}");
+
+                        output = new PSObject(result);
+                        break;
+                }
+
+                WriteObject(output, true);
+            }
         }
 
         protected override void EndProcessing()
         {
-            WriteVerbose("Disconnecting GremlinClient");            
+            WriteVerbose("Disconnecting GremlinClient");
             Client.Dispose();
         }
 
--- test/Test-Cosmos.ps1
+++ test/Test-Cosmos.ps1
@@ -4,12 +4,14 @@ param (
     [PSCredential] $creds = (Get-Credential "/dbs/dev01/colls/ldm")
 )
 
-
 Import-Module $(Join-Path $PSScriptRoot "../Publish/PSGremlin.dll")
 Get-Command -Module PSGremlin
 
 $queries = @(
-    "g.V().has('tenantId','ditchwitch')", "g.E().has('tenantId','ditchwitch')"
+    "g.V().has('tenantId','ditchwitch')", 
+    "g.E().has('tenantId','ditchwitch')"
 )
 
-$queries | Invoke-Gremlin -Hostname "dev01-graph01.gremlin.cosmosdb.azure.com" -Credential $creds -EnableSsl $true | ConvertTo-Json -Depth 5
+$queries |
+    Invoke-Gremlin -Hostname "dev01-graph01.gremlin.cosmosdb.azure.com" -Credential $creds -EnableSsl |
+    ConvertTo-Json -Depth 5
